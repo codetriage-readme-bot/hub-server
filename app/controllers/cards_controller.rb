b12bc42750c3119965e1cff2a42c398c3a3d5612
Nirmalya Ghosh <nirmalya.email@gmail.com>
2016-06-27 08:25:20 +0530

    made the all cards api

---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,14 @@
+class CardsController < ApplicationController
+  def index
+    @cards = Card.all
+    render json: @cards
+  end
+
+  def show
+    @card = Card.find(params[:id])
+    render json: @card
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
 
   resources :users
-
+  resources :cards
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
