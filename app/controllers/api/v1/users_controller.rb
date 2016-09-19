b35dfcab2756671415ff698088a304a19efc476e
Nirmalya Ghosh <nirmalya.email@gmail.com>
2016-09-19 07:47:06 +0530

    feat(app): add edit option for registered users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,6 +13,21 @@ module Api
 
       def create
       end
+
+      def update
+        @user = User.find(params[:id])
+        if @user.update_attributes(user_params)
+          render json: @user
+        else
+          render json: @user, status: 422
+        end
+      end
+
+      private
+
+      def user_params
+        params.require(:data).require(:attributes).permit(:name, :email)
+      end
     end
   end
 end
