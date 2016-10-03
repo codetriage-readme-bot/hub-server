8bb2b90d520f130a98089f383ee3ecc36cd78ad6
Nirmalya Ghosh <nirmalya.email@gmail.com>
2016-10-03 10:15:31 +0530

    enhancement(app): add the created_at attribute to the api

--- app/serializers/card_serializer.rb
+++ app/serializers/card_serializer.rb
@@ -1,5 +1,5 @@
 class CardSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :type, :project_id, :slug, :status
+  attributes :id, :title, :description, :type, :project_id, :slug, :status, :created_at
 
   has_many :users
   belongs_to :project
